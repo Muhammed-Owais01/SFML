375c5dd0683f3144a1e3cf8077c890771027e67f
Muhammed-Owais01 <workandrecovery@gmail.com>
2024-03-09 19:46:30 +0500

    Enemy Created

--- OOP Project Files/OOP Project/Enemy.cpp
+++ OOP Project Files/OOP Project/Enemy.cpp
@@ -1,1 +1,48 @@
 #include "Enemy.h"
+
+void Enemy::initVariables()
+{
+	this->enemy.setFillColor(sf::Color::Yellow);
+	this->enemy.setSize(sf::Vector2f(10.f, 10.f));
+}
+
+Enemy::Enemy()
+{
+	this->initVariables();
+}
+
+Enemy::~Enemy()
+{
+
+}
+
+void Enemy::updateMovement()
+{
+	this->enemy.move(this->getVelocity());
+}
+
+void Enemy::updateWindowCollision(sf::RenderWindow& window)
+{
+	sf::FloatRect bounds = this->enemy.getGlobalBounds();
+	if (bounds.top < 0.f)
+		this->enemy.setPosition(this->enemy.getPosition().x, 0.f);
+	else if (bounds.top + bounds.height > window.getSize().y)
+		this->enemy.setPosition(this->enemy.getPosition().x, window.getSize().y - bounds.height);
+
+	if (bounds.left < 0.f)
+		this->enemy.setPosition(0.f, this->enemy.getPosition().y);
+	else if (bounds.left + bounds.width > window.getSize().x)
+		this->enemy.setPosition(window.getSize().x - bounds.width, this->enemy.getPosition().y);
+}
+
+void Enemy::update(sf::RenderWindow& window)
+{
+	this->updateMovement();
+	this->updatePhysics();
+	this->updateWindowCollision(window);
+}
+
+void Enemy::render(sf::RenderTarget* target)
+{
+	target->draw(this->enemy);
+}
--- OOP Project Files/OOP Project/Enemy.h
+++ OOP Project Files/OOP Project/Enemy.h
@@ -1,12 +1,22 @@
 #pragma once
+
+#include "Platform.h"
+
 class Enemy
+	: public GameEngine
 {
 private:
+	sf::RectangleShape enemy;
+
+	void initVariables();
 public:
 	Enemy();
 	~Enemy();
 
-	void update();
-	void render();
+	void updateMovement();
+	void updateWindowCollision(sf::RenderWindow& window);
+
+	void update(sf::RenderWindow& window);
+	void render(sf::RenderTarget* target);
 };
 
